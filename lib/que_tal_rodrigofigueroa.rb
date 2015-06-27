85b85cd286a1e9a7520ae4616b3b8a4f197ee26e
bidah <bartoindahouse@gmail.com>
2015-06-27 12:50:25 -0400

    new version with instance method como_estas?

--- lib/que_tal_rodrigofigueroa.rb
+++ lib/que_tal_rodrigofigueroa.rb
@@ -1,5 +1,15 @@
 require "que_tal_rodrigofigueroa/version"
 
 module QueTalRodrigofigueroa
-  # Your code goes here...
+  class QueTal
+    self.como_estas?(mood)
+      if mood == "bien"
+        puts "que bueno amigow"
+      elsif mood == "mal"
+        puts "que mal amigow. animo!"
+      else
+        puts "Te puedo responder si me dices si estas 'bien' o 'mal' solamente."
+      end
+    end
+  end
 end
